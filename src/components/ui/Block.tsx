d279bf23d41d4be72ed3945f924ae029ad1b48fe
aetchego <alizeetche@gmail.com>
2025-01-31 02:07:26 +0100

    block

--- src/components/ui/Block.tsx
+++ src/components/ui/Block.tsx
@@ -6,9 +6,11 @@ type BlockProps = {
   color?: string;
 };
 
-const Block = ({ title, children, color = "bg-gray-50" }: BlockProps) => {
+const Block = ({ title, children, color = "bg-white" }: BlockProps) => {
   return (
-    <div className={`${color} rounded-lg p-5 h-full flex flex-col`}>
+    <div
+      className={`${color} rounded-lg p-5 h-full flex flex-col border-solid border-[1px] border-gray-100`}
+    >
       {title && <h2 className="text-lg font-bold mb-4">{title}</h2>}
       <div className="flex-1">{children}</div>
     </div>
